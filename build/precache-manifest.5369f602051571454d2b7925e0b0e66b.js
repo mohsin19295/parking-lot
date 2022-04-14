3849e0873f1c047205b096ca486ea62c4ee72032
mohsin19295 <mohsin19295@gmail.com>
2022-04-14 07:17:42 +0530

    build added

---
+++ build/precache-manifest.5369f602051571454d2b7925e0b0e66b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acde04a004ea0e6929f86c06099e8d7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fb5351c118d03c6e283",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "b8a47d32befd30f4745b",
+    "url": "/static/css/main.5826c266.chunk.css"
+  },
+  {
+    "revision": "2fb5351c118d03c6e283",
+    "url": "/static/js/2.d4c1f565.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.d4c1f565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a47d32befd30f4745b",
+    "url": "/static/js/main.c85681f7.chunk.js"
+  },
+  {
+    "revision": "461d1ec57b23cc872dbd",
+    "url": "/static/js/runtime-main.26944662.js"
+  }
+]);
